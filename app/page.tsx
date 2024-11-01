4f495d8dcd35ba40d1dc8b2dfe3a059081646ec1
Esteban Castro <estcascor94@hotmail.com>
2024-10-31 22:31:28 -0600

    fix boiler plate

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
+'use client'
+
 import React from "react";
 
 import Welcome from "@/components/welcome";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
   const router = useRouter();
--- components/provider.tsx
+++ components/provider.tsx
@@ -2,7 +2,6 @@
 
 import { Provider } from "react-redux";
 import store from "@/store/store";
-import "../styles/globals.css";
 
 export default function Providers({ children }: React.PropsWithChildren) {
   return <Provider store={store}>{children}</Provider>;
