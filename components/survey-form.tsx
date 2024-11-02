33669086509935721a599185ff541a636281723a
criscasguitar <criscascor9@gmail,com>
2024-11-01 19:30:50 -0600

    UI x3

--- components/survey-form.tsx
+++ components/survey-form.tsx
@@ -12,6 +12,8 @@ import {
 
 import { Label } from "./ui/label";
 import Data from "@/content/survey-options.json";
+import { Input } from "./ui/input";
+
 
 interface Survey {
   label: string;
@@ -20,13 +22,15 @@ interface Survey {
   type: "select" | "input";
 }
 
+console.log(Data)
+
 export function SurveyForm() {
-  const SelectComp = ({ survey }: { survey: Survey }, key: string) => {
+  const SelectComp = ({ survey }: { survey: Survey }) => {
     if (survey.type !== "select") return null;
 
     return (
-      <div className="flex flex-col justify-start" key={key}>
-        <Label className="mb-1">{survey.label}</Label>
+      <div className="flex flex-col justify-start mb-10">
+        <Label className="mb-2 leading-1">{survey.label}</Label>
         <Select>
           <SelectTrigger className="w-full">
             <SelectValue placeholder="Selecciona una opcion" />
@@ -35,7 +39,7 @@ export function SurveyForm() {
             <SelectGroup>
               {survey.options && survey.options.length
                 ? survey.options.map((option, i) => (
-                    <SelectItem key={`option-${i}-${key}`} value={option.text}>
+                    <SelectItem key={`option-${i}`} value={option.text}>
                       {option.text}
                     </SelectItem>
                   ))
@@ -47,17 +51,33 @@ export function SurveyForm() {
     );
   };
 
+  const InputComponent = ({survey} : {survey: Survey}) => {
+    if(survey.type !== 'input') return null;
+
+    return (
+      <div className="flex flex-col justify-start mb-10">
+        <Label className="mb-4">{survey.label}</Label>
+        <Input placeholder="Ingrese su respuesta" className="w-full" type={survey.format}/>
+
+      </div>
+    )
+    
+  }
+
   return (
     <div>
-      <h2>Encuesta de deseos</h2>
-      <p>
+      <h2 className="font-bold item text-xl mb-4">Encuesta de deseos</h2>
+      <p className="text-sm mb-3 from-accent-foreground">
         Aca te encuentras una lista donde tienes que escoger la opcion con la
         que mas te identifiques
       </p>
       {Data.map((data, i) => (
-        <>
-          <SelectComp survey={data as Survey} key={`survey-question-${i}`} />
-        </>
+        <div key={`survey-question-${i}`}>
+          <SelectComp survey={data as Survey} />
+          <InputComponent survey={data as Survey}/>
+        </div>
+          
+       
       ))}
     </div>
   );
